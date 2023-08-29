88c3db36cc2151c9118d51ea338f2e88e4759728
Конончук Андрей <andrey@MacBook-Air-Kononcuk.local>
2023-08-29 22:16:10 +0300

    Fixed cleaning error

--- src/components/erroralert/ErrorAlert.jsx
+++ src/components/erroralert/ErrorAlert.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Alert } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { setError } from "../../reducers/postsReducer";
 
 const ErrorAlert = ({ message }) => {
+  const dispatch = useDispatch();
   const [show, setShow] = useState(true);
+
+  function onCloseHandler() {
+    setShow(false);
+    dispatch(setError(""));
+  }
 
   if (show) {
     return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+      <Alert variant="danger" onClose={onCloseHandler} dismissible>
         <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
         <p>{message}</p>
       </Alert>
